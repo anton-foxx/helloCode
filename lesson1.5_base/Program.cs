f7de012f835f76c93b01add45b20dfc22fad2afd
Anton <1@example.com>
2022-09-16 12:06:38 +0300

    add lesson1.4 _Base

---
+++ lesson1.5_base/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = 4;
+int b = 10;
+int c = 11;
+int d = 3;
+int e = 5;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write(" max= ");
+Console.Write(max);
+    
